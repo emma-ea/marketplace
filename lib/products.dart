d69324e2483ec5c169aeb785a469e1e3d03c4ce8
Emmanuel Awuah <awuahemmanuel56@gmail.com>
2019-11-24 19:15:27 +0000

    optimised listview, added widget to display if data was passed.

--- lib/products.dart
+++ lib/products.dart
@@ -1,26 +1,39 @@
 import 'package:flutter/material.dart';
 
 class Products extends StatelessWidget {
-
   final List<String> products;
 
   Products([this.products = const []]);
 
+  Widget _buildProductItem(BuildContext context, int index) {
+    return Card(
+      child: Column(
+        children: <Widget>[
+          Image.asset('assets/images/food.jpg'),
+          Text(products[index]),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildProductList() {
+    Widget productCards;
+    if (products.length > 0) {
+      productCards = ListView.builder(
+        itemCount: products.length,
+        itemBuilder: _buildProductItem,
+      );
+    } else {
+      productCards = Center(
+        child: Text('No Products Found.'),
+      );
+    }
+    return productCards;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: products
-          .map(
-            (element) => Card(
-              child: Column(
-                children: <Widget>[
-                  Image.asset('assets/images/food.jpg'),
-                  Text(element),
-                ],
-              ),
-            ),
-          )
-          .toList(),
-    );
+    print('[Products Widget] build()');
+    return _buildProductList();
   }
 }
